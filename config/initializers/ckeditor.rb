450572d7d7f9129b25845f435c2cfa1fcb79bd24
Niall Quinn <niall@niallquinn.me>
2017-04-25 11:06:30 +0100

    Reduce CKE compilation time

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -55,4 +55,9 @@ Ckeditor.setup do |config|
   # Used when CKEditor CDN enabled
   # By default: "/assets/ckeditor/config.js"
   # config.js_config_url = "/assets/ckeditor/config.js"
+  #
+  Ckeditor.setup do |config|
+    config.assets_languages = ['en', 'fr']
+    config.assets_plugins = ['image', 'smiley']
+  end
 end
