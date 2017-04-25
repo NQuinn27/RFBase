13f28334139e8ad170cc85e7b24b7928ca9c224c
Niall Quinn <niall@niallquinn.me>
2017-04-25 12:19:36 +0100

    move cke to bootsy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = true
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.serve_static_assets = false
   config.assets.compile = false # we don't want compilation fallbacks
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
