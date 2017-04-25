8da32b3c66c02aeb7eb309deaa8b712320cbdad7
Niall Quinn <niall@niallquinn.me>
2017-04-25 11:21:13 +0100

    Remove cke override

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require ckeditor/override
 //= require ckeditor/init
 //= require_tree .
 //= require jquery.minicolors
