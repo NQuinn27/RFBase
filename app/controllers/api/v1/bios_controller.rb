46b8d214940f4699931787d3dedc785a6ead396d
Niall Quinn <niall@niallquinn.me>
2017-04-28 15:42:50 +0100

    extend customer apikey for all

--- app/controllers/api/v1/bios_controller.rb
+++ app/controllers/api/v1/bios_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BiosController < Api::V1::ApiController
-  before_action :load_and_verify_authenticity_token
-  before_action :current_customer
+  before_action :load_and_verify_authenticity_token, only: [:index]
+  before_action :current_customer, only: [:index]
 
   def index
     @bio = Bio.where(:customer_id => @current_customer.id).first
